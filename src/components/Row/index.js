0f5fd2be0f2a58ff4b10b02477bbb9cc82a6e765
Vaibhav <varora@catalystone.com>
2020-08-18 22:23:27 +0530

    fix: 🐛 app breaking due to deleted file being called

--- src/components/Row/index.js
+++ src/components/Row/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import Content from './Content';
-import './Row.css';
 import { row } from './Row.style';
 
 const Row = (props) => {
-
     return (
         <div
             draggable={true}
