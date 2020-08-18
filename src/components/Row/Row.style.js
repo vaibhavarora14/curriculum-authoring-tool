37a7bc5953efaaf307b94507cf121aad8de99232
Vaibhav <varora@catalystone.com>
2020-08-18 22:10:59 +0530

    refactor: 💡 unnecessary code from row style file

--- src/components/Row/Row.style.js
+++ src/components/Row/Row.style.js
@@ -1,56 +1,6 @@
-export const icon = {
-    fill: '#BDBDBD',
-    marginRight: '0.25rem',
-    fontWeight: 700,
-    cursor: 'pointer',
-}
-
-export const icon__disabled = {
-    fill: '#EBEBEB',
-}
-
-export const icon__arrow = {
-    width: '2rem',
-    height: '2rem'
-}
-
-export const icon__trash = {
-    width: '1.5rem',
-    height: '1.5rem',
-    marginRight: '1rem'
-}
-
 export const row = {
     display: 'flex',
     alignItems: 'center',
     borderBottom: '1px solid #E0E0E0',
     cursor: 'move'
-}
-
-export const row__over__border = '3px dotted #666';
-
-export const row__contentWrapper = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: '1'
-}
-
-export const pointer = {
-    display: "flex",
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FAFAFA',
-    color: '#E0E0E0',
-    padding: '1rem 1.25rem',
-    marginRight: '3rem',
-    userSelect: 'none'
-}
-
-export const textInput = {
-    flex: '1',
-    cursor: 'pointer',
-    border: '0',
-    padding: '1rem',
-    fontSize: '1rem'
 }
